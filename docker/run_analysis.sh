5517635f84518a7aeea733ddcfb868a0fa5a32fb
Ana Trisovic <ana.trisovic@gmail.com>
2020-07-16 10:40:56 -0400

    handle no files and add 3hr time limit

--- docker/run_analysis.sh
+++ docker/run_analysis.sh
@@ -5,43 +5,21 @@ doi="$1" # get DOI
 python2 download_dataset.py "$doi"
 #sleep 10
 #echo "a <- 10 + 2" > temp.R
-ln -s /lib/x86_64-linux-gnu/libreadline.so.7.0 /lib/x86_64-linux-gnu/libreadline.so.6
+
+# only needed for R 3.2.1
+#ln -s /lib/x86_64-linux-gnu/libreadline.so.7.0 /lib/x86_64-linux-gnu/libreadline.so.6
 
 # process all R files and collect data
 python2 set_environment.py $PWD
 
-# execute R files
-Rscript exec_r_files.R "$doi"
-# conda run -n py3 python execute_files.py 
-# python execute_files.py "$doi"
+# execute R files with 3 hour limit
+timeout 3h Rscript exec_r_files.R "$doi"
 
-FILE=run_log.csv
-if [ -f "$FILE" ]; then
-     echo "$FILE exists"
-else
-     echo "$doi,unknown,other error" > run_log.csv
-fi
-
-FILEDS=run_log_ds.csv
-if [ -f "$FILEDS" ]; then
-     echo "$FILEDS exists"
-else
-     echo "$doi,unknown,download error" > run_log_ds.csv
-fi
-
-FILES=run_log_st.csv
-if [ -f "$FILES" ]; then
-     echo "$FILES exists"
-else
-     echo "0,0,0,download error" > run_log_st.csv
-fi
-
-FILEST=run_log_st1.csv
-if [ -f "$FILEST" ]; then
-     echo "$FILEST exists"
-else
-     echo "download error,0,0,0,0,0" > run_log_st1.csv
-fi
+# note if 3hr time limit exceeded 
+if [ $? -eq 124 ]
+ then
+     echo "$doi,unknown,time limit exceeded" >> run_log.csv
+ fi
 
 # send results 
 python2 save_result_in_dynamo.py "$doi"
--- docker/save_result_in_dynamo.py
+++ docker/save_result_in_dynamo.py
@@ -1,5 +1,6 @@
-import boto3
 import csv
+import boto3
+import os.path
 from datetime import datetime
 
 import sys
@@ -9,94 +10,106 @@ session = boto3.session.Session(region_name= 'us-east-2')
 dynamodb = session.resource('dynamodb')
 
 # table 1: success or error
-table1 = dynamodb.Table("run_log_32")
 
-items = []
-with open('run_log.csv') as csv_file:
-	reader = csv.reader(csv_file)
-	for rows in reader:
-        	mydict = {
-            		'doi':rows[0],
-            		'filename': rows[1],
-            		'error': rows[2],
-					'date_time':datetime.now().isoformat()
-        			}
-        	items.append(mydict)
-	print(items)
+if os.path.isfile('run_log.csv'): 
+	table1 = dynamodb.Table("run_log_36")
+	items = []
 
+	with open('run_log.csv') as csv_file:
+		reader = csv.reader(csv_file)
+		for rows in reader:
+				mydict = {
+						'doi':rows[0],
+						'filename': rows[1],
+						'error': rows[2],
+						'date_time':datetime.now().isoformat()
+						}
+				items.append(mydict)
+		print(items)
 
-with table1.batch_writer() as batch:
-	for item in items:
-		batch.put_item(Item=item)
+	'''
+	with table1.batch_writer() as batch:
+		for item in items:
+			batch.put_item(Item=item)
+	'''
 
 
 # table 2: checksum check
-table2 = dynamodb.Table("run_log_download_32")
 
-items = []
-with open('run_log_ds.csv') as csv_file:
-	reader = csv.reader(csv_file)
-	for rows in reader:
-        	mydict = {
-            		'doi':rows[0],
-            		'fileid':rows[1],
-            		'status':rows[2],
-        	}
-        	items.append(mydict)
-	print(items)
+if os.path.isfile('run_log_ds.csv'):
+	table2 = dynamodb.Table("run_log_download_36")
+	items = []
+
+	with open('run_log_ds.csv') as csv_file:
+		reader = csv.reader(csv_file)
+		for rows in reader:
+				mydict = {
+						'doi':rows[0],
+						'fileid':rows[1],
+						'status':rows[2],
+				}
+				items.append(mydict)
+		print(items)
+
+	'''
+	with table2.batch_writer() as batch:
+		for item in items:
+			batch.put_item(Item=item)
+	'''
 
 
-with table2.batch_writer() as batch:
-	for item in items:
-		batch.put_item(Item=item)
-
-"""
 # table 3: in file info
-table3 = dynamodb.Table("run_log_stats")
 
-items = []
-with open('run_log_st.csv') as csv_file:
-	reader = csv.reader(csv_file)
-	for rows in reader:
-        	mydict = {
-				    'doi':doi,
-            		'comments_no':rows[0],
-            		'dependen_no':rows[1],
-            		'total_size':rows[2],
-            		'list_of_all':rows[3],
-					'list_of_libs':rows[4]
-        	}
-        	items.append(mydict)
-	print(items)
+if os.path.isfile('run_log_st.csv'):
+	table3 = dynamodb.Table("run_log_stats")
+	items = []
 
+	with open('run_log_st.csv') as csv_file:
+		reader = csv.reader(csv_file)
+		for rows in reader:
+				mydict = {
+						'doi':doi,
+						'comments_no':rows[0],
+						'dependen_no':rows[1],
+						'total_size':rows[2],
+						'list_of_all':rows[3],
+						'list_of_libs':rows[4]
+				}
+				items.append(mydict)
+		print(items)
 
-with table3.batch_writer() as batch:
-	for item in items:
-		batch.put_item(Item=item)
+	'''
+	with table3.batch_writer() as batch:
+		for item in items:
+			batch.put_item(Item=item)
+	'''
 
 # table 4: in file info per file
-table4 = dynamodb.Table("run_log_file_stats")
 
-items = []
-with open('run_log_st1.csv') as csv_file:
-	reader = csv.reader(csv_file)
-	for rows in reader:
-        	mydict = {
-				    'doi':doi,
-					'filename':rows[0],
-					'total_lines':rows[1],
-            		'comments_no':rows[2],
-            		'dep_no':rows[3],
-					'func_no':rows[4],
-					'test_no':rows[5],
-					'class_no':rows[6],
-					'encoding':rows[7],
-					'confidence':rows[8]
-        	}
-        	items.append(mydict)
-	print(items)
+if os.path.isfile('run_log_st1.csv'):
+	table4 = dynamodb.Table("run_log_file_stats")
+	items = []
 
-with table4.batch_writer() as batch:
-	for item in items:
-		batch.put_item(Item=item)
-"""
+	with open('run_log_st1.csv') as csv_file:
+		reader = csv.reader(csv_file)
+		for rows in reader:
+				mydict = {
+						'doi':doi,
+						'filename':rows[0],
+						'total_lines':rows[1],
+						'comments_no':rows[2],
+						'dep_no':rows[3],
+						'func_no':rows[4],
+						'test_no':rows[5],
+						'class_no':rows[6],
+						'encoding':rows[7],
+						'confidence':rows[8]
+				}
+				items.append(mydict)
+		print(items)
+
+	'''
+	with table4.batch_writer() as batch:
+		for item in items:
+			batch.put_item(Item=item)
+	'''
